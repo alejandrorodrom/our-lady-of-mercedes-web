cde722641aface82de22898d06da4a1511287e07
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2023-10-18 05:43:14 -0500

    feature: add header (80%), add button component (100%)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './core/layouts/header/header.component';
+import { NgOptimizedImage } from "@angular/common";
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/layouts/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  readonly logoImage: string = 'https://lh6.googleusercontent.com/xrgU08r3-dClIcJdLoGHtah3s0OMsfNXu6OIADrt30w57iM8QoBydMXKJXysR0_Uq663CVkyAmUzk4ceGOEG334=w16383'
+}
