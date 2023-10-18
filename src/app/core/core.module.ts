076dfdef216629b1d6fa62395dfebbcca7f1cf83
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2023-10-18 11:11:48 -0500

    feature: add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from './layouts/header/header.component';
 import { SharedModule } from "../shared/shared.module";
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
